07106862862ff2d1859641153a905dbb3478a8ef
Guilherme Muniz Cardoso <guilhermemunizcardoso@gmail.com>
2018-08-20 23:36:50 -0300

    First published lib example.

--- app/src/main/java/guilherme/cardoso/customviewlibrary/MainActivity.java
+++ app/src/main/java/guilherme/cardoso/customviewlibrary/MainActivity.java
@@ -3,11 +3,15 @@ package guilherme.cardoso.customviewlibrary;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import guilherme.cardoso.swipeprofilecard.Logger;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new Logger().error("Teste");
     }
 }
---
+++ swipeprofilecard/src/main/java/guilherme/cardoso/swipeprofilecard/Logger.java
@@ -0,0 +1,16 @@
+package guilherme.cardoso.swipeprofilecard;
+
+import android.util.Log;
+
+public class Logger {
+
+    private final String TAG = "Logger";
+
+    public void error(String message) {
+        error(TAG, message);
+    }
+
+    public void error(String tag, String message) {
+        Log.e(tag, message);
+    }
+}
